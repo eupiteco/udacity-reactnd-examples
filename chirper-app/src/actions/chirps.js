db4d5a6e6c3016abba3acd571079e68ce1a8b7b1
lucas.piteco <lucas.piteco@nextt.com.br>
2019-03-14 18:21:01 -0300

    WIP chirp actions

--- chirper-app/src/actions/chirps.js
+++ chirper-app/src/actions/chirps.js
@@ -1,6 +1,7 @@
-import { saveLikeToggle } from "../utils/api"
+import { saveLikeToggle, saveChirp } from "../utils/api"
 export const RECEIVE_CHIRPS = "RECEIVE_CHIRPS"
 export const TOGGLE_LIKE = "TOGGLE_LIKE"
+export const ADD_CHIRP = "ADD_CHIRP"
 
 export function receiveChirps(chirps) {
 	return {
@@ -18,6 +19,16 @@ function toggleLike({ id, authedUser, hasLiked }) {
 	}
 }
 
+function addChirp({id, text, author, replyingTo }) {
+	return {
+		type: ADD_CHIRP,
+		id,
+		text,
+		author,
+		replyingTo
+	}
+}
+
 export function handleToggleLike (data) {
 	return (dispatch) => {
 		dispatch(toggleLike(data))
@@ -27,6 +38,21 @@ export function handleToggleLike (data) {
 				console.warn('Error, in handleToggleLike: ', e)
 				dispatch(toggleLike(data))
 				alert('There was an error liking the tweet. Try again.')
+			})
+	}
+}
+
+export function handleAddChirp (data) {
+	return (dispatch) => {
+		return saveChirp(data)
+			.then((res) => {
+				const {id} = res
+				data.id = id
+				dispatch(addChirp(data))
+			})
+			.catch((e) => {
+				console.warn('Error, in handleToggleLike: ', e)
+				alert('There was a problem adding a new tweet, please try again.')
 			})
 	}
 }
--- chirper-app/src/reducers/chirps.js
+++ chirper-app/src/reducers/chirps.js
@@ -1,4 +1,4 @@
-import { RECEIVE_CHIRPS, TOGGLE_LIKE } from "../actions"
+import { RECEIVE_CHIRPS, TOGGLE_LIKE, ADD_CHIRP } from "../actions"
 
 export default function chirps ( state = {}, action ) {
 	switch( action.type ) {
@@ -17,6 +17,11 @@ export default function chirps ( state = {}, action ) {
 						? state[id].likes.filter((uid) => uid !== authedUser)
 						: state[id].likes.concat([authedUser])
 				}
+			}
+		case ADD_CHIRP:
+			const { text, author, replyingTo } = action
+			return {
+
 			}
 		default: return state
 	}
