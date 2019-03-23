0d11c87cad9ffc61d682708b7e05825877da3755
lucas.piteco <lucas.piteco@nextt.com.br>
2019-03-23 12:18:21 -0300

    app conectado recebendo posts e categorias

--- projeto-redux/frontend/src/components/App.js
+++ projeto-redux/frontend/src/components/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-import {getCategories} from '../utils/ReadableAPI';
+import { connect } from 'react-redux';
 import '../assets/App.css';
+import {handleInitialData} from '../actions/shared';
 import Dashboard from './Dashboard'
 
 class App extends Component {
 
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
   render() {
     return (
       <div className="App">
@@ -14,4 +18,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
--- projeto-redux/frontend/src/components/Dashboard.js
+++ projeto-redux/frontend/src/components/Dashboard.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import {getInitialData} from '../utils/ReadableAPI';
 
 class Dashboard extends React.Component {
-  state = {
-    posts: [],
-    categories: [],
-  };
-  componentDidMount() {
-    this.handleInitialdata();
-  }
   render() {
-		console.log(this.state)
     return (
       <div>
         <h1>Dashboard</h1>
       </div>
     );
   }
-  handleInitialdata = () => {
-    getInitialData().then(res => {
-      const {posts, categories} = res;
-      this.setState(() => ({
-        posts,
-        categories,
-      }));
-    });
-  };
 }
 
 export default Dashboard;
